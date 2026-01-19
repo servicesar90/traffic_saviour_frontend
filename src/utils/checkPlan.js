fc69a60b266efad09c85d00768e1a300b8314676
servicesar89 <servicesar89@gmail.com>
2026-01-19 17:37:04 +0530

    check plan

--- src/utils/checkPlan.js
+++ src/utils/checkPlan.js
@@ -2,10 +2,10 @@ export const isPlanValid = () => {
   const plan = JSON.parse(localStorage.getItem("plan"));
 
   if (!plan) return false;
-  if (plan.status !== "Paid") return false;
+  if (!plan.isActive) return false;
 
   const today = new Date();
-  const endDate = new Date(plan.end_date);
+  const endDate = new Date(plan.endDate);
 
   return today <= endDate;
 };
